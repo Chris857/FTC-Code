3dde026399f96054d30f332ae5de41acdb3ce123
Chris D <chrisd857@gmail.com>
2016-10-05 00:01:43 -0400

    Initial commit of color sensor class and a test that uses two color sensors.

---
+++ src/main/java/org/firstinspires/ftc/teamcode/MultiplexColorSensorTest.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by FTC 10723 on 10/4/2016
+ *
+ * In this example, you need to create a device configuration that lists two
+ * "I2C Device"s, one named "mux" and the other named "ada". There are two
+ * Adafruit color sensors plugged into the I2C multiplexer on ports 0 and 3.
+ */
+@TeleOp(name="MultiplexColorSensorTest", group="Iterative Opmode")
+public class MultiplexColorSensorTest extends OpMode
+{
+    MultiplexColorSensor muxColor;
+    int[] ports = {0, 3};
+
+    @Override
+    public void init() {
+        int milliSeconds = 48;
+        muxColor = new MultiplexColorSensor(hardwareMap, "mux", "ada",
+                                            ports, milliSeconds,
+                                            MultiplexColorSensor.GAIN_16X);
+    }
+
+    @Override
+    public void init_loop() {
+
+    }
+
+    @Override
+    public void start() {
+        muxColor.startPolling();
+    }
+
+    @Override
+    public void loop() {
+        for (int i=0; i<ports.length; i++) {
+            int[] crgb = muxColor.getCRGB(ports[i]);
+
+            telemetry.addLine("Sensor " + ports[i]);
+            telemetry.addData("CRGB", "%5d %5d %5d %5d",
+                              crgb[0], crgb[1], crgb[2], crgb[3]);
+        }
+
+        telemetry.update();
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
---
+++ src/main/java/org/firstinspires/ftc/teamcode/readme.md
@@ -0,0 +1,5 @@
+MultiplexColorSensor is a class which allows you to use multiple Adafruit color
+sensors (https://www.adafruit.com/products/1334, TCS34725) with the Adafruit
+I2C multiplexer (https://www.adafruit.com/product/2717, TCA9548A).
+
+MultiplexColorSensorTest provides an example of using MultiplexColorSensor.
